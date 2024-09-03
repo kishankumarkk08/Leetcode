ec9f2cc5665e64e1b54d92b067cce2ae639848b2
Kishan Kumar <129841590+kishankumarkk08@users.noreply.github.com>
2024-09-03 13:32:58 +0530

    Time: 88 ms (10.53%), Space: 55.2 MB (95.51%) - LeetHub

---
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        int res[] = new int[nums.length];
+        PriorityQueue <Integer> pq = new PriorityQueue<>();
+        for(int i = 0; i < nums.length; i++) {
+            pq.add(nums[i]);
+        }
+        for(int i = 0; i < res.length; i++) {
+            res[i] = pq.remove();
+        }
+        return res;
+        
+    }
+}
