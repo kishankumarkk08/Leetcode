edff603dd91f9249ce0be326659a19e70b67e168
Kishan Kumar <129841590+kishankumarkk08@users.noreply.github.com>
2024-10-18 00:05:21 +0530

    Time: 9 ms (21.16%), Space: 44.7 MB (22.22%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        ArrayList<Integer> a = new ArrayList<>();
+        for (int i = 0; i < lists.length; i++) {
+            ListNode curr = lists[i];
+            while (curr != null) {
+                a.add(curr.val);
+                curr = curr.next;
+            }
+        }
+        Collections.sort(a);
+        ListNode h = new ListNode(-1);
+        ListNode curr = h;
+        for (int i = 0; i < a.size(); i++) {
+            ListNode tmp = new ListNode(a.get(i));
+            curr.next = tmp;
+            curr = curr.next;
+        }
+        return h.next;
+    }
+}
