dab86cd324a770c473d7a3c80a60649beaeb51c9
Kishan Kumar <129841590+kishankumarkk08@users.noreply.github.com>
2024-12-12 00:36:50 +0530

    Time: 275 ms (17.6%), Space: 158.4 MB (61.22%) - LeetHub

--- 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.java
@@ -1,29 +1,37 @@
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        Map <Integer, List<Integer>> graph = new HashMap();
-        for(int [] edge : edges) {
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        for (int[] edge : edges) {
             int u = edge[0], v = edge[1];
-            graph.computeIfAbsent(u, value->new ArrayList<>()).add(v);
-            graph.computeIfAbsent(v, value->new ArrayList<>()).add(u);
+            graph.computeIfAbsent(u, value -> new ArrayList<>()).add(v);
+            graph.computeIfAbsent(v, value -> new ArrayList<>()).add(u);
         }
-
         boolean vis[] = new boolean[n];
-        for(int i = 0; i < vis.length; i++) vis[i] = false;
+        for (int i = 0; i < vis.length; i++) {
+            vis[i] = false;
+            
+        }
         vis[source] = true;
 
-        Queue<Integer> q = new LinkedList<>();
-        q.add(source);
-        while(!q.isEmpty()) {
-            int curr = q.remove();
-            if(curr == destination) return true;
-            for(int neighbor : graph.get(curr)) {
-                if(!vis[neighbor]) {
-                    q.add(neighbor);
-                    vis[neighbor] = true;
+        return dfs(graph, source, destination, vis);
+
+
+    }
+
+    private boolean dfs(Map<Integer, List<Integer>> graph, int source, int destination, boolean vis[]) {
+        if(source == destination) {
+            return true;
+        }
+        for(int neighbor : graph.get(source)) {
+            if(!vis[neighbor]) {
+                vis[neighbor] = true;
+                if(dfs(graph, neighbor, destination, vis)) {
+                    return true;
                 }
             }
         }
-
         return false;
     }
+    
+
 }
