108a50d75aa719bbfbd299a4e000378b79ee1e7f
Kishan Kumar <129841590+kishankumarkk08@users.noreply.github.com>
2024-12-27 16:51:27 +0530

    Time: 4 ms (21.16%), Space: 47.2 MB (72.69%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,36 +1,34 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        ArrayList<ArrayList<Integer>> graph = new ArrayList<ArrayList<Integer>>();
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+        boolean vis[] = new boolean[isConnected.length];
+        Arrays.fill(vis, false);
         for(int i = 0; i < isConnected.length; i++) {
             graph.add(new ArrayList<>());
         }
         for(int i = 0; i < isConnected.length; i++) {
             for(int j = 0; j < isConnected.length; j++) {
-                if(isConnected[i][j] == 1 && i != j) {
-                    graph.get(i).add(j);
+                if(isConnected[i][j] == 1) {
+                    graph.get(i).add(i);
                     graph.get(j).add(i);
                 }
             }
         }
-
-        boolean vis[] = new boolean[isConnected.length];
-        Arrays.fill(vis, false);
         int count = 0;
-        for(int i = 0; i < graph.size(); i++) {
+        for(int i = 0; i < isConnected.length; i++) {
             if(!vis[i]) {
                 count++;
-                bfs(i,isConnected, graph, vis);
+                bfs(i, isConnected, vis, graph);
             }
         }
         return count;
-
     }
-    public void bfs(int start,int[][] isConnected, ArrayList<ArrayList<Integer>> graph , boolean[] vis) {
+    public void bfs(int start, int[][] isConnected, boolean[] vis, ArrayList<ArrayList<Integer>> graph) {
         Queue <Integer> q = new LinkedList<>();
         q.add(start);
         while(!q.isEmpty()) {
             int curr = q.remove();
-            for(int neighbors : graph.get(curr)) {
+            for(int neighbors: graph.get(curr)) {
                 if(!vis[neighbors]) {
                     vis[neighbors] = true;
                     q.add(neighbors);
@@ -38,5 +36,4 @@ class Solution {
             }
         }
     }
-
 }
